8aaa50c8fca94e7966058a51383ac6a1fab696a6
rbscloud <rasmus@rbscloud.dk>
2012-03-14 13:39:05 +0100

    The noc_N.vhd is now a double torus and uses tiles.

--- noc/vhdl/noc_N.vhd
+++ noc/vhdl/noc_N.vhd
@@ -6,42 +6,44 @@ use work.noc_types.all;
 
 entity noc is
   -- NxN nodes
-  generic (N         : natural := 4;
-            WIDTH    : natural := 16;
-            PERIOD_P : natural := 5
-            );
+  generic (N        : natural := 8;
+           WIDTH    : natural := 16;
+           PERIOD_P : natural := 28
+           );
   port(
-    p_clk : in std_logic;
-    n_clk : in std_logic;
+    processor_clk : in std_logic;
+    router_clk : in std_logic;
     reset : in std_logic;
 
-    data_in  : in  std_logic_vector(WIDTH-1 downto 0);
-    data_out : out std_logic_vector(WIDTH-1 downto 0)
+    ser_txd : out std_logic;
+    ser_rxd : in  std_logic
+    --data_in  : in  std_logic_vector(WIDTH-1 downto 0);
+    --data_out : out std_logic_vector(WIDTH-1 downto 0)
 
     );
 end entity noc;
 
 architecture struct of noc is
 
-  component noc_node
-    generic (WIDTH     : natural;
-              PERIOD_P : natural);
-    port (p_clk       : in std_logic;
-                n_clk : in std_logic;
-                reset : in std_logic;
+  --Component noc_node
+  --  generic (WIDTH    : natural;
+  --           PERIOD_P : natural);
+  --  port (p_clk : in std_logic;
+  --        n_clk : in std_logic;
+  --        reset : in std_logic;
 
-                north_in : in network_link_forward;
-                south_in : in network_link_forward;
-                east_in  : in network_link_forward;
-                west_in  : in network_link_forward;
+  --        north_in : in network_link_forward;
+  --        south_in : in network_link_forward;
+  --        east_in  : in network_link_forward;
+  --        west_in  : in network_link_forward;
 
-                north_out : out network_link_forward;
-                south_out : out network_link_forward;
-                east_out  : out network_link_forward;
-                west_out  : out network_link_forward
+  --        north_out : out network_link_forward;
+  --        south_out : out network_link_forward;
+  --        east_out  : out network_link_forward;
+  --        west_out  : out network_link_forward
 
-                );
-  end component;
+  --        );
+  --end component;
 
   type link_n is array(0 to (N - 1)) of network_link_forward;
   type link_m is array(0 to (N - 1)) of link_n;
@@ -61,43 +63,69 @@ begin
   -- generate connections
   nodes_m : for i in N-1 downto 0 generate
     nodes_n : for j in N-1 downto 0 generate
-      node : noc_node
-        generic map (WIDTH    => WIDTH,
-                     PERIOD_P => PERIOD_P)
-        port map (p_clk     => p_clk, n_clk => n_clk,
-                  reset     => reset,
-                  north_in  => north_in(i)(j),
-                  south_in  => south_in(i)(j),
-                  east_in   => east_in(i)(j),
-                  west_in   => west_in(i)(j),
-                  north_out => north_out(i)(j),
-                  south_out => south_out(i)(j),
-                  east_out  => east_out(i)(j),
-                  west_out  => west_out(i)(j)
-                  );
+      output_node : if i = 0 and j = 0 generate
+        node : entity work.tile
+          generic map (
+            UART          => true,
+            TOTAL_NI_NUM  => N*N,
+            NI_NUM        => i*N+j,
+            stable_length => PERIOD_P)
+          port map (router_clk => router_clk, processor_clk => processor_clk,
+                    reset      => reset,
+                    north_in   => north_in(i)(j),
+                    south_in   => south_in(i)(j),
+                    east_in    => east_in(i)(j),
+                    west_in    => west_in(i)(j),
+                    north_out  => north_out(i)(j),
+                    south_out  => south_out(i)(j),
+                    east_out   => east_out(i)(j),
+                    west_out   => west_out(i)(j),
+                    ser_txd    => ser_txd,
+                    ser_rxd    => ser_rxd
+                    );
+      end generate output_node;
+
+      normal_node : if i /= 0 or j /= 0 generate
+        node : entity work.tile
+          generic map (
+            UART          => false,
+            TOTAL_NI_NUM  => N*N,
+            NI_NUM        => i*N+j,
+            stable_length => PERIOD_P)
+          port map (router_clk => router_clk, processor_clk => processor_clk,
+                    reset      => reset,
+                    north_in   => north_in(i)(j),
+                    south_in   => south_in(i)(j),
+                    east_in    => east_in(i)(j),
+                    west_in    => west_in(i)(j),
+                    north_out  => north_out(i)(j),
+                    south_out  => south_out(i)(j),
+                    east_out   => east_out(i)(j),
+                    west_out   => west_out(i)(j),
+                    ser_txd    => open,
+                    ser_rxd    => '0'
+                    );
+      end generate normal_node;
     end generate nodes_n;
   end generate nodes_m;
 
-  --outinterface
-  north_in(0)(0) <= data_in;
-  data_out       <= north_out(0)(0);
+  ----outinterface
+  --north_in(0)(0) <= data_in;
+  --data_out       <= north_out(0)(0);
 
   --interconnections    
-  open_vector <= (others => '0');
+  open_vector.data <= (others => '0');
   links_m : for i in 0 to N-1 generate
     links_n : for j in 0 to N-1 generate
-      open_link_north : if ((i = 0) and not(j = 0)) generate
-        north_in(i)(j) <= open_vector;
-      end generate open_link_north;
-      open_link_south : if (i = N) generate
-        south_in(i)(j) <= open_vector;
-      end generate open_link_south;
-      open_link_west : if (j = 0) generate
-        west_in(i)(j) <= open_vector;
-      end generate open_link_west;
-      open_link_east : if (j = N) generate
-        east_in(i)(j) <= open_vector;
-      end generate open_link_east;
+
+      link_north_south : if (i = 0) generate
+        north_in(i)(j)   <= south_out(N-1)(j);
+        south_in(N-1)(j) <= north_out(i)(j);
+      end generate link_north_south;
+      link_west_east : if (j = 0) generate
+        west_in(i)(j)   <= east_out(i)(N-1);
+        east_in(i)(N-1) <= west_out(i)(j);
+      end generate link_west_east;
 
       bottom : if (i = (N-1) and j < (N-1)) generate
         west_in(i)(j+1) <= east_out(i)(j);
